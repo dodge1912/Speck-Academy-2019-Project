7d918dcfd5a46f94e5f388d30bad333f5bbf5a08
dodge1912 <ivang1912@gmail.com>
2019-03-27 23:13:51 +0100

    add 2 of 4 reserved halls, add module.exports

---
+++ halls.js
@@ -0,0 +1,9 @@
+var list = [
+    {name: "Dvorana1",Id: 1,reservation: {isReserved: true,reservedFrom: new Date("2019-03-20"),reservedUntil: new Date("2019-03-28"),},},
+    {name: "Dvorana1",Id: 2,reservation: {isReserved: true,reservedFrom: new Date("2019-03-20"),reservedUntil: new Date("2019-03-28"),},},
+    {name: "Dvorana3",Id: 3,reservation: {isReserved: false,reservedFrom: null,reservedUntil: null,},},
+    {name: "Dvorana4",Id: 4,reservation: {isReserved: false,reservedFrom: null,reservedUntil: null,},},
+    
+];
+
+module.exports = list;
